4d94ecba8193bd83c762024044d4f42122d183c7
albertng94 <albertng94@gmail.com>
2024-04-29 19:14:40 +0200

    Lift state up and create ResultsTable component skeleton.
    Lifted state up from "UserInputSection" to the "App" component so that
    the updated user data input can be successfully passed onto the new
    "ResultsTable" component.
    
    Created the ResultsTable component skeleton.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
 import UserInputSection from "./components/UserInput/UserInputSection.jsx"
+import ResultsTable from "./components/Results/ResultsTable.jsx"
+
+const investmentData = {
+  initialInvestment: "",
+  annualInvestment: "",
+  expectedReturn: "",
+  duration: ""
+};
+
 
 function App() {
+
+  const [userData, setUserData] = useState(investmentData);
+
+    function handleInputChange(event) {
+        setUserData((prevData) => {
+            let updatedData = {...prevData};
+            console.log(updatedData);
+            for (let key in updatedData) {
+                if (event.target.className === key) {
+                    updatedData[key] = event.target.value;
+                    console.log(updatedData);
+                }
+            }
+            console.log(updatedData);
+            return updatedData;
+        });
+    }
+
+
   return (
-    <UserInputSection />
+    <>
+      <UserInputSection onChange={handleInputChange}/>
+      <ResultsTable />
+    </>
   )
 }
 
